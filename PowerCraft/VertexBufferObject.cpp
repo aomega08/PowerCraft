57f9e83df2fb18707c23c78dbcf32882c8177042
Francesco Boffa <fra.boffa@gmail.com>
2015-10-25 12:59:08 +0100

    Added VBO abstraction class

---
+++ PowerCraft/VertexBufferObject.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+#include "VertexBufferObject.h"
+
+VertexBufferObject::VertexBufferObject() {
+  glGenBuffers(1, &_id);
+}
+
+VertexBufferObject::~VertexBufferObject() {
+  glDeleteBuffers(1, &_id);
+}
+
+void VertexBufferObject::Bind() {
+  glBindBuffer(GL_ARRAY_BUFFER, _id);
+}
+
+void VertexBufferObject::Upload(void *data, size_t size, int type) {
+  glBufferData(GL_ARRAY_BUFFER, size, data, type);
+}
---
+++ PowerCraft/VertexBufferObject.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "stdafx.h"
+
+class VertexBufferObject {
+public:
+  VertexBufferObject();
+  ~VertexBufferObject();
+
+  void Bind();
+  void Upload(void *data, size_t size, int type = GL_STATIC_DRAW);
+
+private:
+  GLuint _id;
+};
